47eb805378198dc15fe6b295799b02e5ab231885
BeycanDeveloper <halilbeycan0@gmail.com>
2026-01-16 17:17:17 +0800

    parse to units

--- src/app/api/cst-price/route.ts
+++ src/app/api/cst-price/route.ts
@@ -1,7 +1,6 @@
+import { parseUnits } from "ethers";
+
 export function GET() {
-    const oneUsdcInCst = 2;
-    return new Response(
-        JSON.stringify(oneUsdcInCst),
-        { status: 200 },
-    );
+    const oneUsdcInCst = Number(parseUnits("2", 6));
+    return new Response(JSON.stringify(oneUsdcInCst), { status: 200 });
 }
